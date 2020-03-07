673a638f9da4df8b9a12aa0ebcf95e3e4c1a5a21
rishibodake <rishibodake@gmail.com>
2020-03-07 17:45:57 +0530

    Added:LuckyUnluckyFeature

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -2,20 +2,23 @@
 #Discription:Gambling Simulation As Per UseCases
 #Author:Hrishikesh M Bodake
 #Since:March 7 2020 Saturday
-
 stakes=100
 day=20
-game=0
 isWon=1
 winningGoal=150
 loosingLimit=50
 noOfBets=5
+lCounter=0
+luckyDay=0
+unLuckyDay=0
+maxWinCount=0
+maxLostCount=0
 totalAmount=0
 totalLost=0
 #UC5:echo the loss or won per month
 
 function playForDays(){
-	 for (( index=0; index<$day; index++ ))
+	 for (( index=1; index<=$day; index++ ))
    do
       stakes=100
        while [[ $stakes -gt $loosingLimit && $stakes -lt $winningGoal ]]
@@ -23,13 +26,26 @@ function playForDays(){
          if [[ $((RANDOM%2)) -eq $isWon ]]
          then
             stakes=$(($stakes+1))
+				((wCounter++))
          else
             stakes=$(($stakes-1))
+				((lCounter++))
          fi
        done
+	totalAmount=$(($totalAmount+$stakes))
 
-   totalAmount=$(($totalAmount+$stakes))
+	if [[ $wCounter > $maxWinCount ]]
+	then
+		maxWinCount=$wCounter
+		luckyDay=$index
+	elif [[ $lCounter > $maxLostCount ]]
+	then
+		maxLostCount=$lCounter
+		unLuckyDay=$index
+	fi
    done
+	echo "Lucky day => $luckyDay"
+	echo "Unlucky day => $unLuckyDay"
    investedAmount=$(($day*100))
    if [[ $(($totalAmount-$investedAmount)) -lt 0 ]]
    then
