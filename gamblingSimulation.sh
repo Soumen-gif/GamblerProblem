0b72bde2fb685655008e420dec7c232e83831fa9
rishibodake <rishibodake@gmail.com>
2020-03-07 17:14:12 +0530

    Refactor:Made the Changes

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -13,32 +13,31 @@ noOfBets=5
 totalAmount=0
 totalLost=0
 #UC5:echo the loss or won per month
-function gamble(){
-		while [[ $stakes -gt $loosingLimit && $stakes -lt $winningGoal ]]
-		do
-			if [[ $((RANDOM%2)) -eq $isWon ]]
-			then
-				stakes=$(($stakes+1))
-			else
-				stakes=$(($stakes-1))
-			fi
-				((game++))
-		done
-		echo $stakes
-}
+
 function playForDays(){
-	for (( index=0; index<$day; index++ ))
-	do
-		totalAmount=$(($totalAmount+$( gamble )))
-	done
-	investedAmount=$(($day*100))
+	 for (( index=0; index<$day; index++ ))
+   do
+      stakes=100
+       while [[ $stakes -gt $loosingLimit && $stakes -lt $winningGoal ]]
+      do
+         if [[ $((RANDOM%2)) -eq $isWon ]]
+         then
+            stakes=$(($stakes+1))
+         else
+            stakes=$(($stakes-1))
+         fi
+       done
+
+   totalAmount=$(($totalAmount+$stakes))
+   done
+   investedAmount=$(($day*100))
    if [[ $(($totalAmount-$investedAmount)) -lt 0 ]]
    then
       lost=$(($investedAmount-$totalAmount))
-		echo "Total lost this month are: $lost"
+      echo "Total lost this month are: $lost"
    else
       won=$(($totalAmount-$investedAmount))
-		echo "Total win this month are: $won"
+      echo "Total win this month are: $won"
    fi
 
 }
