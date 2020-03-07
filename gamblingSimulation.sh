9095441462a2f7d597dbebdb37ce26c6a10d9ee8
rishibodake <rishibodake@gmail.com>
2020-03-07 14:11:37 +0530

    Added:PlayingForDaysFeature

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -4,23 +4,32 @@
 #Since:March 7 2020 Saturday
 
 stakes=100
-day=0
+day=20
 game=0
 isWon=1
 winningGoal=150
 loosingLimit=50
 noOfBets=5
-#UC2:As calculative gambler if won or loose 50% of stakes it will be end of day
+totalWon=0
+totalLost=0
+#UC4:Play for 20 days and calculate total won or lost
 function gamble(){
-	while [[ $stakes -gt $loosingLimit && $stakes -lt $winningGoal ]]
+		while [[ $stakes -gt $loosingLimit && $stakes -lt $winningGoal ]]
+		do
+			if [[ $((RANDOM%2)) -eq $isWon ]]
+			then
+				stakes=$(($stakes+1))
+			else
+				stakes=$(($stakes-1))
+			fi
+				((game++))
+		done
+		echo $stakes
+}
+function playForDays(){
+	for (( index=0; index<$day; index++ ))
 	do
-		if [[ $((RANDOM%2)) -eq $isWon ]]
-		then
-			stakes=$(($stakes+1))
-		else
-			stakes=$(($stakes-1))
-		fi
-			((game++))
+		totalWon=$(($totalWon+$( gamble )))
 	done
 }
-gamble
+playForDays
